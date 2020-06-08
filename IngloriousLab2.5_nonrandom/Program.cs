224682c7f7f794520c445e54c7589e18cffc5109
thetridenter <kostia.perevertailo@icloud.com>
2020-06-09 01:30:49 +0300

    done

---
+++ IngloriousLab2.5_nonrandom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IngloriousLab2._5_nonrandom
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("Введіть розмірність масиву");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] a = new int[n,n];
+
+            Console.WriteLine("Елементи масиву:");
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("Введіть елемент №" + i + "," + j);
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+
+            int p = 1;
+            for (int i = n - 1; i > -1; i--)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    p *= a[i, j];
+                }
+            }
+
+            Console.WriteLine("\n Добуток бічної діагоналі = " + p);
+        }
+    }
+}
